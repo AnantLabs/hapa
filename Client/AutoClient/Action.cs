91da79b2c54aa485a61b93b6bd7f95d7234ca9c5
pangyazhi <pangyazhi@gmail.com>
2012-04-26 17:14:21 +0100

    aha

--- Client/AutoClient/Action.cs
+++ Client/AutoClient/Action.cs
@@ -8,5 +8,17 @@ namespace AutoClient
 {
     public class Action
     {
+        public void Do()
+        {
+            //prepare ui objects in browser
+
+            //pageobjects input (input (will not cause page changed) / verify)
+
+            //take snapshot here 
+            //pageobjects action (an action cause the page changed)
+
+            //clear ui objects in browser
+            //prepare the result
+        }
     }
 }
---
+++ Client/SeleniumActions/Browser.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+using Selenium;
+using Common;
+using System.Diagnostics;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System.Collections;
+
+namespace SeleniumActions
+{
+    public class Browser
+    {
+        private static Browser Instance = null;
+        private IWebDriver browser;
+        private Browser()
+        {
+        }
+        public static Browser GetInstance()
+        {
+            return Instance ?? (Instance = new Browser());
+        }
+
+        public IWebDriver GetCurrentBrowser()
+        {
+            if (browser == null)
+            {
+                CloseBrowser();
+                StartBrowser();
+            }
+            DismissUnexpectedAlert();
+            List<string> bs = browser.WindowHandles;
+            if (bs.Count == 1)
+            {
+                browser.SwitchTo().Window(bs[0]);
+            }
+            else
+            {
+                browser.SwitchTo().Window(bs[bs.Count - 1]);
+            }
+
+            return browser;
+        }
+        //CSStype subpool<fingerprint string, webelement>
+        Hashtable pool = new Hashtable();
+        //CSS types that we care
+        string CSSXPath = Configuration.Settings("CSSType","//*[@class='ROW1' or @class='ROW2' or @class='' or @class='' or @class='' or @class='' or @class='' or @class='' or @class='' or @class='' or @class='' or @class='']");
+        public void Prepare()
+        {
+
+        }
+
+        public void Clear()
+        {
+
+        }
+
+        public IWebElement GetWebElement(string tag, params string[] key_value)
+        {
+            //TODO get object from object pool? Multithread?
+        }
+
+        public string Snapshot()
+        {
+            IJavaScriptExecutor js = GetCurrentBrowser() as IJavaScriptExecutor;
+            Response screenshotResponse = js.ExecuteScript(DriverCommand.Screenshot, null);
+            return screenshotResponse.Value.ToString();
+        }
+
+        private void DismissUnexpectedAlert()
+        {
+            IAlert alert = GetAlert();
+            if (alert != null)
+            {
+                alert.Dismiss();
+                Logger.GetInstance().Log().Warn("Close an unexpected dialog, please check.");
+            }
+        }
+
+        private IAlert GetAlert()
+        {
+            IAlert alert = null;
+            try
+            {
+                alert = browser.SwitchTo().Alert();
+            }
+            catch (Exception ex)
+            {
+                Logger.GetInstance().Log().Debug("Suppress get alert");
+            }
+            return alert;
+        }
+
+        private void StartBrowser()
+        {
+            string browserType = Configuration.Settings("BrowserType", "IE");
+            if (browserType.Equals("IE"))
+                browser = new InternetExplorerDriver();
+            if (browserType.Equals("Firefox"))
+                browser = new FirefoxDriver();
+            if (browserType.Equals("Chrome"))
+                browser = new ChromeDriver();
+
+            MaximiseBrowser();
+        }
+
+        private void MaximiseBrowser()
+        {
+            browser.Manage().Window.Maximize();
+        }
+
+        private void CloseBrowser()
+        {
+            if (browser != null)
+                browser.Quit();
+            browser = null;
+
+            string browserType = Configuration.Settings("BrowserType", "IE");
+            if (browserType.Equals("IE"))
+                Process.Start("taskkill /IM iexplore.exe");
+            if (browserType.Equals("Firefox"))
+                Process.Start("taskkill /IM firefox.exe");
+            if (browserType.Equals("Chrome"))
+                Process.Start("taskkill /IM chrome.exe");
+
+        }
+    }
+}
