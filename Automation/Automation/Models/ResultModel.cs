fd987e00ff2d0b4648e8928ab0ffbefa2756be3c
huangjien <huangjien@gmail.com>
2012-04-26 20:43:46 +0100

    office network issue

---
+++ Automation/Automation/Models/ResultModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Automation.Models
+{
+    public class ResultModel
+    {
+    }
+
+    public class SnapshotModel 
+    {
+        [Required]
+        public string Id { get; set; }
+        public string Snapshot { get; set; }
+    }
+}
