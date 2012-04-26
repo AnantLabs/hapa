29ae1be9057cc7c7d51966bbd1f90190127ecc0d
huangjien <huangjien@gmail.com>
2012-04-26 20:48:42 +0100

    office network issue

---
+++ Common/Common/Snapshot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common
+{
+    public class Snapshot : SuperData
+    {
+        public string base64String { get; set; }
+    }
+}
